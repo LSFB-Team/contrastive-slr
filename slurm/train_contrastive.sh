020591fc95d58e8e02e4d924f064696b64d6b8e6
Jérôme Fink <jerome.fink@unamur.be>
2024-06-26 10:07:44 +0200

    run on debug

--- slurm/train_contrastive.sh
+++ slurm/train_contrastive.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 # Submission script for Lucia
 #SBATCH --job-name=backbone_lsfb
-#SBATCH --time=30:00:00 # hh:mm:ss
+#SBATCH --time=00:10:00 # hh:mm:ss
 #
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=4
 #SBATCH --gres="gpu:1"
 #SBATCH --mem-per-cpu=6144 
-#SBATCH --partition=ia
+#SBATCH --partition=debug-gpu
 #
 #SBATCH --mail-user=jerome.fink@unamur.be
 #SBATCH --mail-type=ALL
 #
 #SBATCH --account=lsfb
 #
-#SBATCH --output=./outputs/backbone_multi_LS.out
+#SBATCH --output=./outputs/test.out
 
 module purge
-module load Python/3.10.4-GCCcore-11.3.0
+module load EasyBuild/2023a
+module load Python/3.11.3-GCCcore-12.3.0
 
 source ./venv/bin/activate
 pip install -r requirements.txt
